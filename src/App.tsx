5506230ac60e704767491a703adce8ab3351c5ce
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-06-20 20:46:46 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 // import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Router />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
